eaff25d8e523ba7a6e6cc62e0a3d3e093b58b68e
John Wang <jzw2@illinois.edu>
2022-10-10 21:29:29 +0200

    Add basic graphs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use serde_json::{Result, Value};
 use serde::{Deserialize};
-
+use graphviz_rust::dot_structures::*;
+use graphviz_rust::dot_generator::*;
+use graphviz_rust::{exec, parse, print};
+use graphviz_rust::cmd::{CommandArg, Format};
+use graphviz_rust::printer::{PrinterContext,DotPrinter};
+use graphviz_rust::attributes::*;
 
 #[derive(Deserialize, Debug)]
 enum Kind {
@@ -37,6 +42,105 @@ struct Phon {
 
 }
 
+
+fn make_graph(phons: Vec<Phon>) -> Graph {
+
+    let id = id!("identity");
+    //let p = NodeId();
+    let mut graph = graph!(id!("id"); node!("SentenceTop"),
+                           node!("ClauseTop"),
+                           node!("CoreTop"),
+                           node!("NucTop"),
+edge!(node_id!("SentenceTop") => node_id!("ClauseTop") => node_id!("CoreTop") => node_id!("NucTop"))
+    );
+
+    for (index, p) in phons.iter().enumerate() {
+        graph.add_stmt(Stmt::Node(node!(index.to_string(); attr!("label", esc p.phon))));
+        if let Some(t) = &p.top {
+            let pos_name = index.to_string() + "pos";
+            graph.add_stmt(node!(pos_name; attr!("label", esc t.pos)).into());
+            graph.add_stmt(edge!(node_id!(pos_name) => node_id!(index.to_string()) => node_id!("CoreTop")).into());
+
+        }
+    }
+
+    let x = graph.print(&mut PrinterContext::default());
+    println!("{}",x );
+
+
+
+return graph;
+
+}
+
+    static T1: &str = r#"
+
+[
+    {
+        "phon" : "Will",
+        "bot" : [
+
+            {
+                "op" : "IF",
+                "kind": "Clause"
+            },
+            {
+                "op" : "TNS",
+                "kind": "Clause"
+            }
+        ]
+
+    },
+    {
+    "phon" : "they",
+    "top" :
+            {
+                "pos" : "NP",
+                "kind": "Core"
+            }
+    },
+    {
+        "phon" : "have to",
+        "bot" : [
+
+            {
+                "op" : "MOD",
+                "kind": "Core"
+            }
+        ]
+
+    },
+    {
+        "phon" : "be",
+        "bot" : [
+
+            {
+                "op" : "ASP",
+                "kind": "Nuc"
+            }
+        ]
+
+    },
+    {
+        "phon" : "be",
+        "top" :
+                {
+                    "pos" : "V",
+                    "kind": "Pred"
+                },
+        "bot" : [
+
+            {
+                "op" : "ASP",
+                "kind": "Nuc"
+            }
+        ]
+
+    }
+
+]
+
+"#;
 
 fn untyped_example() -> Result<()> {
     // Some JSON input data as a &str. Maybe this comes from the user.
@@ -91,10 +195,204 @@ fn untyped_example() -> Result<()> {
     let v: Phon = serde_json::from_str(top_test)?;
 
     println!("Tetsing {:?}", v);
+
+    let top_test = r#"
+    {
+    "phon" : "did"
+}
+
+"#;
+
+    let v: Phon = serde_json::from_str(top_test)?;
+
+    println!("Tetsing {:?}", v);
+
+
+    let top_test = r#"
+    {
+        "phon" : "Will",
+        "bot" : [
+
+            {
+                "op" : "IF",
+                "kind": "Clause"
+            },
+            {
+                "op" : "TNS",
+                "kind": "Clause"
+            }
+        ]
+
+    }
+
+"#;
+
+    let v: Phon = serde_json::from_str(top_test)?;
+
+    println!("Tetsing {:?}", v);
+
+
+    let top_test = r#"
+[
+    {
+    "phon" : "what",
+    "top" : {
+        "pos": "NP",
+        "kind": "Clause"
+        }
+    },
+    {
+    "phon" : "did"
+},
+    {
+    "phon" : "Robin",
+    "top" : {
+        "pos" : "NP",
+        "kind" : "Core"
+    }
+
+},
+    {
+    "phon" : "show",
+    "top" : {
+        "pos" : "V",
+        "kind": "Pred"
+    }
+},
+    {
+    "phon" : "to Pat",
+    "top" : {
+        "pos" : "PP",
+        "kind": "Core"
+    }
+},
+    {
+    "phon" : "in the library",
+    "top" :
+    {
+        "pos" : "PP",
+        "kind": "CoreP"
+    }
+},
+    {
+    "phon" : "yesterday",
+    "top" :
+    {
+        "pos" : "ADV",
+        "kind": "CoreP"
+    }
+}
+
+]
+"#;
+
+    let v: Vec<Phon> = serde_json::from_str(top_test)?;
+
+    println!("Tetsing {:?}", v);
+
+
+
+    let top_test = r#"
+
+[
+    {
+        "phon" : "Will",
+        "bot" : [
+
+            {
+                "op" : "IF",
+                "kind": "Clause"
+            },
+            {
+                "op" : "TNS",
+                "kind": "Clause"
+            }
+        ]
+
+    },
+    {
+    "phon" : "they",
+    "top" :
+            {
+                "pos" : "NP",
+                "kind": "Core"
+            }
+    },
+    {
+        "phon" : "have to",
+        "bot" : [
+
+            {
+                "op" : "MOD",
+                "kind": "Core"
+            }
+        ]
+
+    },
+    {
+        "phon" : "be",
+        "bot" : [
+
+            {
+                "op" : "ASP",
+                "kind": "Nuc"
+            }
+        ]
+
+    },
+    {
+        "phon" : "be",
+        "top" :
+                {
+                    "pos" : "V",
+                    "kind": "Pred"
+                },
+        "bot" : [
+
+            {
+                "op" : "ASP",
+                "kind": "Nuc"
+            }
+        ]
+
+    }
+
+]
+
+"#;
+
+    println!("hi");
+    let v: Result<Vec<Phon>> = serde_json::from_str(top_test);
+    match v {
+        Err(x) => {
+            println!("error: {:?}", x);
+        }
+        Ok(_) => println!("test test {:?}", v),
+    }
+
+
+
     Ok(())
 }
 
+fn draw_stuff() {
+    untyped_example();
+
+
+
+ let mut g = make_graph(serde_json::from_str(T1).unwrap());
+
+       let graph_svg = exec(g, &mut PrinterContext::default(), vec![
+           CommandArg::Format(Format::Svg),
+            CommandArg::Output("test.svg".to_string())
+       ]).unwrap();
+
+    println!("making graph");
+    println!("{}", graph_svg);
+    println!("making graph");
+}
 
 fn main() {
-    untyped_example();
+    //untyped_example();
+    draw_stuff();
 }
