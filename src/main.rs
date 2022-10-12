652759b39c0ec1409924b7bb4f412f2233c2f8f1
John Wang <jzw2@illinois.edu>
2022-10-12 20:54:18 +0200

    Cargo format

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
-
-use std::fmt::Display;
-use std::fmt::format;
 use std::env;
+use std::fmt::format;
+use std::fmt::Display;
 use std::fs::read_to_string;
 
-use serde_json::{Result, Value};
-use serde::{Deserialize};
-use graphviz_rust::dot_structures::*;
-use graphviz_rust::dot_generator::*;
-use graphviz_rust::{exec, parse, print};
-use graphviz_rust::cmd::{CommandArg, Format};
-use graphviz_rust::printer::{PrinterContext,DotPrinter};
 use graphviz_rust::attributes::*;
+use graphviz_rust::cmd::{CommandArg, Format};
+use graphviz_rust::dot_generator::*;
+use graphviz_rust::dot_structures::*;
+use graphviz_rust::printer::{DotPrinter, PrinterContext};
+use graphviz_rust::{exec, parse, print};
+use serde::Deserialize;
+use serde_json::{Result, Value};
 
 #[derive(Deserialize, Debug, Clone)]
 enum Kind {
@@ -22,11 +21,10 @@ enum Kind {
     Clause,
     ClauseP,
     Sentence,
-
 }
 
 impl Kind {
-    fn to_string(&self) -> String{
+    fn to_string(&self) -> String {
         let s = match &self {
             Kind::Pred => "Pred",
             Kind::Nuc => "Nuc",
@@ -50,14 +48,13 @@ impl Kind {
 
 #[derive(Deserialize, Debug, Clone)]
 struct Top {
-     pos: String,
-     kind: Kind,
+    pos: String,
+    kind: Kind,
 }
 
 impl Top {
     fn make_edge(&self, node: usize) -> Stmt {
-
-        let pos_node = node_id!(self.pos.clone() + &node.to_string() );
+        let pos_node = node_id!(self.pos.clone() + &node.to_string());
         // let pos_node = node_id!(node.to_string() + &self.pos);
         //
         let connect_node_string = if let Some(_) = self.kind.get_per() {
@@ -72,10 +69,9 @@ impl Top {
 
 #[derive(Deserialize, Debug)]
 struct Bot {
-     op: String,
-     kind: Kind,
+    op: String,
+    kind: Kind,
 }
-
 
 #[derive(Deserialize, Debug)]
 struct Phon {
@@ -83,13 +79,10 @@ struct Phon {
     // not included in the input.
     phon: String,
     top: Option<Top>,
-    bot: Option<Vec<Bot>>
-
+    bot: Option<Vec<Bot>>,
 }
 
-
 fn make_graph(phons: Vec<Phon>) -> Graph {
-
     let mut graph = graph!(di id!("id");
                            node!("SentenceTop"; attr!("label", "Sentence"), attr!("group", "main")),
                            node!("ClauseTop"; attr!("label", "Clause"), attr!("group", "main")),
@@ -103,9 +96,6 @@ fn make_graph(phons: Vec<Phon>) -> Graph {
                            node!("PredBot"; attr!("label", "Pred"), attr!("group", "main"))
     );
 
-
-
-
     // assume there can only be one
     let mut pred_index = None;
 
@@ -113,21 +103,21 @@ fn make_graph(phons: Vec<Phon>) -> Graph {
     for (index, p) in phons.iter().enumerate() {
         let mut add_main = false;
         if let Some(t) = &p.top {
-
             let mut a = vec![attr!("label", esc t.pos)];
             if let Kind::Pred = &t.kind {
                 //if it's the pred, we need add the thing to the bottom also
                 pred_index = Some(index);
                 a.push(attr!("group", "main"));
                 add_main = true;
-                graph.add_stmt(node!(t.pos.to_string() + "Bot"; attr!("label", t.pos), attr!("group", "main")).into())
+                graph.add_stmt(
+                    node!(t.pos.to_string() + "Bot"; attr!("label", t.pos), attr!("group", "main"))
+                        .into(),
+                )
             } else {
-
                 graph.add_stmt(t.make_edge(index));
             }
             // add pos
             graph.add_stmt(Stmt::Node(node!(t.pos.to_string() + &index.to_string(), a)));
-
 
             if let Some(pkind) = t.kind.get_per() {
                 let main_name = format!("{}Top", pkind.to_string());
@@ -138,10 +128,8 @@ fn make_graph(phons: Vec<Phon>) -> Graph {
                 let subgr = subgraph!(; attr!("rank", "same"), node!(main_name), node!(p_name));
                 graph.add_stmt(subgr.into());
             }
-
         }
         if let Some(b) = &p.bot {
-
             for (op_index, proj) in b.iter().enumerate() {
                 //let n = node!(proj.kind)
                 let operator_node_name = format!("{}{}_{}", proj.op, index, op_index);
@@ -153,7 +141,6 @@ fn make_graph(phons: Vec<Phon>) -> Graph {
                 let subgr = subgraph!(; attr!("rank", "same"), node!(bottom_name), node!(operator_node_name));
                 graph.add_stmt(subgr.into());
             }
-
         }
         let mut a = vec![attr!("label", esc p.phon)];
         if add_main {
@@ -162,50 +149,51 @@ fn make_graph(phons: Vec<Phon>) -> Graph {
         graph.add_stmt(Stmt::Node(node!(index.to_string(), a)));
     }
 
-
     // make big ventiacl thing in the middle
 
     let mut v = vec![
-
-            node_id!("SentenceTop").into(),
-            node_id!("ClauseTop").into(),
-            node_id!("CoreTop").into(),
-            node_id!("NucTop").into(),
-            node_id!("PredTop").into(),
-
+        node_id!("SentenceTop").into(),
+        node_id!("ClauseTop").into(),
+        node_id!("CoreTop").into(),
+        node_id!("NucTop").into(),
+        node_id!("PredTop").into(),
     ];
 
-    let index = pred_index.expect("Fialure didn't put the pred thing in beaucse I didn't miplemnte it yet");
+    let index =
+        pred_index.expect("Fialure didn't put the pred thing in beaucse I didn't miplemnte it yet");
     let pos = phons[index].top.as_ref().unwrap().pos.to_string();
-    v.push(node_id!(pos.to_string()+ &index.to_string()).into());
+    v.push(node_id!(pos.to_string() + &index.to_string()).into());
     v.push(node_id!(index.to_string()).into());
     v.push(node_id!(pos.to_string() + "Bot").into());
 
     let v_bot = vec![
-
-            node_id!("PredBot").into(),
-            node_id!("NucBot").into(),
-            node_id!("CoreBot").into(),
-            node_id!("ClauseBot").into(),
-            node_id!("SentenceBot").into(),
-
+        node_id!("PredBot").into(),
+        node_id!("NucBot").into(),
+        node_id!("CoreBot").into(),
+        node_id!("ClauseBot").into(),
+        node_id!("SentenceBot").into(),
     ];
     v.extend(v_bot);
 
     //remember to add the bottom stuff here
 
-    let big_vert = Edge{ ty: EdgeTy::Chain(v), attributes: vec![attr!("dir", "none")] };
+    let big_vert = Edge {
+        ty: EdgeTy::Chain(v),
+        attributes: vec![attr!("dir", "none")],
+    };
     graph.add_stmt(big_vert.into());
 
     // make subgraph to make the phon horizontal
     let num_phons = phons.len();
 
     let edges: Vec<_> = (0..num_phons).map(|x| Vertex::from(node_id!(x))).collect();
-    let edge_stmt = Edge{ ty: EdgeTy::Chain(edges), attributes: vec![attr!("style", "invis")] };
+    let edge_stmt = Edge {
+        ty: EdgeTy::Chain(edges),
+        attributes: vec![attr!("style", "invis")],
+    };
     let phons_sub: Subgraph = subgraph!("phons"; attr!("rank", "same"), edge_stmt);
 
     graph.add_stmt(phons_sub.into());
-
 
     // let enumer_top  = phons.iter().enumerate().map(|(num, p)| p.top.as_ref().map( |t| Vertex::from(node_id!(t.pos.to_string() + &num.to_string()))));
 
@@ -214,7 +202,11 @@ fn make_graph(phons: Vec<Phon>) -> Graph {
     //
     // make a subgraph so the pos allign
     let mut v: Vec<Stmt> = vec![attr!("rank", "same").into()];
-    let stmts  = phons.iter().enumerate().map(|(num, p)| p.top.as_ref().map( |t| node!(t.pos.to_string() + &num.to_string()).into()));
+    let stmts = phons.iter().enumerate().map(|(num, p)| {
+        p.top
+            .as_ref()
+            .map(|t| node!(t.pos.to_string() + &num.to_string()).into())
+    });
     let stmts: Vec<_> = stmts.filter_map(|t| t).collect();
     v.extend(stmts);
     let pos_sub: Subgraph = subgraph!("all_pos", v);
@@ -222,29 +214,31 @@ fn make_graph(phons: Vec<Phon>) -> Graph {
     graph.add_stmt(pos_sub.into());
 
     let x = graph.print(&mut PrinterContext::default());
-    println!("{}",x );
+    println!("{}", x);
 
-
-
-return graph;
-
+    return graph;
 }
-
 
 fn main() {
     //untyped_example();
-    let input_file =  env::args().nth(1).expect("Did not provide input file");
-    let output_file =  env::args().nth(2).expect("Did not provide output file");
+    let input_file = env::args().nth(1).expect("Did not provide input file");
+    let output_file = env::args().nth(2).expect("Did not provide output file");
 
     let error = format!("Error in reading file {}", input_file);
 
     let json_content = read_to_string(input_file).expect(&error);
-    let mut g = make_graph(serde_json::from_str(&json_content).expect("You messed up the Json format"));
+    let mut g =
+        make_graph(serde_json::from_str(&json_content).expect("You messed up the Json format"));
 
-       let graph_svg = exec(g, &mut PrinterContext::default(), vec![
-           CommandArg::Format(Format::Svg),
-            CommandArg::Output(output_file)
-       ]).unwrap();
+    let graph_svg = exec(
+        g,
+        &mut PrinterContext::default(),
+        vec![
+            CommandArg::Format(Format::Svg),
+            CommandArg::Output(output_file),
+        ],
+    )
+    .unwrap();
 
     println!("Random Debug info \n ---------- \n {}", graph_svg);
 }
